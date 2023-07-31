f9dc7835015775cdb645da6542374b79055d5221
azimsaidov <137659387+azimsaidov@users.noreply.github.com>
2023-07-31 14:54:48 -0700

    Update source.c

--- source.c
+++ source.c
@@ -4,19 +4,20 @@
 
 // Function to perform selection sort
 void selectionSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        int min_idx = i;
-        for (int j = i + 1; j < n; j++) {
-            if (arr[j] < arr[min_idx]) {
-                min_idx = j;
+      for (int x = 0; x < n - 1; x++) {
+        int minimumIndex = x;
+
+        for (int y = x + 1; y < n; y++) {
+            if (arr[y] < arr[minimumIndex] ) {
+                minimumIndex = y;
             }
         }
 
-        // Swap the found minimum element with the first element
-        int temp = arr[i];
-        arr[i] = arr[min_idx];
-        arr[min_idx] = temp;
+        int temp = arr[x];
+        arr[x] = arr[minimumIndex];
+        arr[minimumIndex] = temp;
     }
+
 }
 
 int main() {
